134ab02c01c7e2949436fb72bf450b4ee49a463f
Joao <fc56311@alunos.fc.ul.pt>
2025-05-22 15:02:40 +0100

    New train pecos, with reduction 13000features

--- src/python/xlinker/train_pecos.py
+++ src/python/xlinker/train_pecos.py
@@ -186,7 +186,9 @@ onnx_directory = "test/test_data/processed/vectorizer/biobert_onnx_cpu.onnx"
 
 start = time.time()
 
-vectorizer_config = {"type": "tfidf", "kwargs": {}}
+max_features = 13000
+
+vectorizer_config = {"type": "tfidf", "kwargs": {"max_features":max_features}}
     
 transformer_config = {
     "type": "biobert",
