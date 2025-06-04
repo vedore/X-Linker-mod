fcacfd0de56c752d4610eb3e04a132289ed3808c
Joao <fc56311@alunos.fc.ul.pt>
2025-06-04 12:50:04 +0100

    max features==3000

--- src/python/xlinker/train_pecos.py
+++ src/python/xlinker/train_pecos.py
@@ -180,13 +180,12 @@ start = time.time()
 
 min_leaf_size = 20
 depth = 2
-n_features = 768
 max_n_clusters = 16
 min_n_clusters = 6
 
 vectorizer_config = {
     "type": "tfidf", 
-    "kwargs": {"max_features":n_features}
+    "kwargs": {}
     }
     
 transformer_config = {
