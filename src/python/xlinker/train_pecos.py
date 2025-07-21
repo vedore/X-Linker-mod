cb8c7cc8cad6dbb5c5675c2591340238eb5479c8
Joao <fc56311@alunos.fc.ul.pt>
2025-07-21 11:33:01 +0100

    .

--- src/python/xlinker/train_pecos.py
+++ src/python/xlinker/train_pecos.py
@@ -183,7 +183,7 @@ vectorizer_config = {
     
 transformer_config = {
     "type": "sentencetbiobert",
-    "kwargs": {"batch_size": 3000, # 4000
+    "kwargs": {"batch_size": 4000, # 4000
                "device": "gpu"}
     }
 
