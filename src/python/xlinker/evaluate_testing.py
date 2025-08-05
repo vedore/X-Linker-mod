27bb97cd75d28e5a8a9d422a5a1abfa367c861ba
Joao <fc56311@alunos.fc.ul.pt>
2025-08-05 12:02:46 +0100

    .

--- src/python/xlinker/evaluate_testing.py
+++ src/python/xlinker/evaluate_testing.py
@@ -135,8 +135,8 @@ gold_labels = read_codes_file("test/test_data/labels_bc5cdr_disease_medic.txt") 
 filtered_labels, filtered_texts = filter_labels_and_inputs(gold_labels, test_input, trained_xtree.initial_labels)
 
 # 10 Counter({0: 1264, 1: 21})
-# 100
-predicted_labels, hits = trained_xtree.predict(filtered_texts, filtered_labels, topk=100)
+# 100 Counter({0: 1244, 1: 41}) Counter({1: 1285}) # Optimal classifier 1 job done
+predicted_labels, hits = trained_xtree.predict(filtered_texts, filtered_labels, topk=100, beam_search=1)
 
 print(predicted_labels, predicted_labels.shape)
     
