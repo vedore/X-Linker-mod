782fe1f1701923b4013c79a2d3d275c1b83202b3
Joao <fc56311@alunos.fc.ul.pt>
2025-08-07 14:43:30 +0100

    .

--- src/python/xlinker/train_pecos.py
+++ src/python/xlinker/train_pecos.py
@@ -331,7 +331,7 @@ xmodel = XModel(vectorizer_config=vectorizer_config,
                 reranker_config=reranker_config,
                 min_leaf_size=min_leaf_size,
                 max_leaf_size=max_leaf_size,
-                n_workers=8,
+                n_workers=10,
                 depth=depth,
                 emb_flag=1
                 )
