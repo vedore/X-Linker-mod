b474a922717270baf9d053b3f8308c1fa817b558
Joao <fc56311@alunos.fc.ul.pt>
2025-09-10 16:00:47 +0100

    .

--- src/python/xlinker/train_pecos.py
+++ src/python/xlinker/train_pecos.py
@@ -331,7 +331,7 @@ min_leaf_size = 5
 max_leaf_size = 200
 cut_half_cluster=True
 ranker_every_layer=True
-depth = 2
+depth = 4
 
 # training_file = os.path.join(os.getcwd(), "test/test_data/train/disease/train_Disease_100.txt")
 
