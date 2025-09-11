4c417a9ebb758049cbbd5194011b9e668d27e13f
Joao <fc56311@alunos.fc.ul.pt>
2025-09-11 19:20:52 +0100

    .

--- src/python/xlinker/evaluate_testing.py
+++ src/python/xlinker/evaluate_testing.py
@@ -128,11 +128,11 @@ with open(test_path, "r") as f:
 
 test_input, test_annots = prepare_input(test_annots_raw, abbreviations, id_2_name)
 
-print(test_input, len(test_input), "\n")
+# print(test_input, len(test_input), "\n")
 
-print(test_annots, len(test_annots))
+# print(test_annots, len(test_annots))
 
-exit()
+# exit()
 
 print("Test instances loaded!")
 
